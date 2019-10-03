a90d200a26be116b53d7d1a26de505d6ea99a421
Roberto H <robhhr@outlook.com>
2019-10-02 18:33:20 -0700

    setup API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { async } from 'q';
 
 const App = () => {
 
   const API = 'd0dcff82abb54485948d11c436e31347';
 
-  const request = `https://api.spoonacular.com/recipes/search?apiKey=${API}`;
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const foodFetch = await fetch(`https://api.spoonacular.com/recipes/search?apiKey=${API}`);
+    const data = await foodFetch.json();
+    console.log(data)
+  }
 
   return (
     <div className="container">
       <form className="search-form">
         <input className="search-bar" type="text" />
-        <button className="search-button" type="submit">Search Recipe...</button>
+        <button
+        className="search-button"
+        type="submit"
+        >
+        Search recipes...
+        </button>
       </form>
+      {/* <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1> */}
     </div>
   )
 }
